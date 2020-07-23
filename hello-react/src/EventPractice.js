0f4f402c07d0235d3f1f197de9ad355692e3ea1f
blair06 <hyewon980610@gmail.com>
2020-07-24 00:14:20 +0900

    함수형 컴포넌트 e.target.name비활용 예시

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const EventPractice = () => {
-    return (
+const EventPractice = ()=> {
+    const[message,setMessage] = useState('');
+    const[username, setUsername] = useState('');
+    const onChangeMessage = (e)=>{setMessage(e.target.value)};
+    const onChangeUsername = (e) => {setUsername(e.target.value)};
+    const onClick = () => {
+        alert(username+ ':' +message);
+        setUsername('');
+        setMessage('');
+    }
+    const onEnter = (e) => {
+        if(e.key = 'Enter'){
+            onClick();
+        }
+    }
+    return(
         <div>
-            
-        </div>
-    );
-};
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          placeholder="username"
+          name="message"
+          onChange={onChangeUsername}/* this는 component */
+          value={username}
+        />
+        <input
+          type="text"
+          placeholder="Message"
+          name="message"
+          onChange={onChangeMessage}/* this는 component */
+          value={message}
+        />
+        <button onClick={onClick}>입력</button>{" "}
+        {/* this는 component */}
+      </div>
+    )
+}
 
 export default EventPractice;
