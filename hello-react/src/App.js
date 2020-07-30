7c7dbf2f51564c2f26522dc0336ccace973ce3db
blair06 <hyewon980610@gmail.com>
2020-07-30 23:09:39 +0900

    Component LifeCycle method

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 
-const App = () => {
-  return <IterationSample/>;
-};
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component{
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () =>{
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+  render(){
+    return(
+      <div>
+        <button onClick = {this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}></LifeCycleSample>
+      </div>
+    )
+  }
+}
+
 export default App;
