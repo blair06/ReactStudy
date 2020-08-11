d1aae764bc14bd186861198ba187efa13a3a9103
blair06 <hyewon980610@gmail.com>
2020-08-12 03:10:00 +0900

    .

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 const App = () => {
+  const [todos, setTodos] =useState([{
+    id:1,
+    text:'리액트 기초 알아보기',
+    checked:true,
+  }, {
+    id:2,
+    text:'리액트 알듯말듯하다..',
+    checked:true,
+  }, {
+    id:3,
+    text:'지금은 새벽3시',
+    checked:false,
+  }]
+  )
   return (
     <TodoTemplate>
       <TodoInsert/>
-      <TodoList/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
     
   );
--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState, useCallback } from 'react';
 import './TodoInsert.scss';
 import {MdAdd} from 'react-icons/md';
 const TodoInsert = () => {
+    const[value,setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
     return (
         <form className="TodoInsert">
-            <input placeholder="할일을 입력하세요"></input>
+            <input placeholder="할일을 입력하세요" value={value} onChange={onChange}></input>
             <button type="submit">
                 <MdAdd/>
             </button>
