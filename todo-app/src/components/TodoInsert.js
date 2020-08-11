bf85fba16f1d2880ad3834ee850d954ee2886b45
blair06 <hyewon980610@gmail.com>
2020-08-12 02:20:03 +0900

    Todo-app basic front

---
+++ todo-app/src/components/TodoInsert.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TodoInsert.scss';
+import {MdAdd} from 'react-icons/md';
+const TodoInsert = () => {
+    return (
+        <form className="TodoInsert">
+            <input placeholder="할일을 입력하세요"></input>
+            <button type="submit">
+                <MdAdd/>
+            </button>
+
+        </form>
+    );
+};
+
+export default TodoInsert;
