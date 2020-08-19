988cd771239c0178acdf59f3bb181cb7d6f50d37
blair06 <hyewon980610@gmail.com>
2020-08-20 02:13:24 +0900

    complete todo-app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,43 +2,67 @@ import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-const App = () => {
-  const [todos, setTodos] =useState([{
-    id:1,
-    text:'리액트 기초 알아보기',
-    checked:true,
-  }, {
-    id:2,
-    text:'리액트 알듯말듯하다..',
-    checked:true,
-  }, {
-    id:3,
-    text:'지금은 새벽3시',
-    checked:false,
-  }]
-  )
 
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  // 고유 값으로 사용 될 id
+  // ref 를 사용하여 변수 담기
   const nextId = useRef(4);
-  const onInsert = useCallback(text => {
-    const todo = {
-      id: nextId.current,
-      text: text,
-      checked:false
-    };
-    setTodos(todos.concat(todo));
-    nextId.current += 1;
-  },[todos],); //두번째 파라미터엔 어떤값이 바뀌었을때 함수를 실행할지 배열로 지정
- 
- const onRemove = useCallback(id=>{
-   setTodos(todos.filter(todo=> todo !== id ));
- },[todos]);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1 씩 더하기
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id)); //todo.id와 id가 같지않은 것들만 배열을 생성해 원본배열을 대체한다.
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+          //todo.id와 id가 같으면 {}을 실행하는데 {}뜻은 todo는 그대로 두고 checked만 todo.checked의 반대로 설정한다.
+        ),
+      );
+    },
+    [todos],
+  );
 
   return (
     <TodoTemplate>
-      <TodoInsert onInsert={onInsert}/>
-      <TodoList todos={todos} onRemove={onRemove}/>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
-    
   );
 };
 
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -6,16 +6,16 @@ import {
   MdRemoveCircleOutline,
 } from "react-icons/md";
 import cn from "classnames";
-const TodoListItem = ({todo, onRemove}) => { //컴포넌트는 props 매개변수를 한개만 받기때문에 객체형으로 받는다.
-    const{id,text, checked} = todo;
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo;
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', {checked})}>
-          {checked ? <MdCheckBox/>:<MdCheckBoxOutlineBlank/>}
+      <div className={cn("checkbox", { checked })} onClick={()=>onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
-      <div className="remove" onClick={()=> onRemove(id)}>
-          <MdRemoveCircleOutline/>
+      <div className="remove" onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
       </div>
     </div>
   );
